41cd1cdd8b959eca667fcaddc06f1e6a1b0dfc6a
jeongjaein <jaein1608@gmail.com>
2023-12-13 21:50:01 +0900

    feat: add Constructor

--- src/main/java/jaein/crudpractice/domain/item/Book.java
+++ src/main/java/jaein/crudpractice/domain/item/Book.java
@@ -12,4 +12,10 @@ public class Book extends Item{
 
     private String author;
     private String isbn;
+
+    public Book(String author, String isbn) {
+        this.author = author;
+        this.isbn = isbn;
+    }
+
 }
