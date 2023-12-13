d89b1ec5a10bdef0b7fafca3ac4547e48d42e2b8
jeongjaein <jaein1608@gmail.com>
2023-12-13 21:48:50 +0900

    feat: add SpringDataJpa

--- src/main/java/jaein/crudpractice/repository/ItemRepository.java
+++ src/main/java/jaein/crudpractice/repository/ItemRepository.java
@@ -1,4 +1,16 @@
 package jaein.crudpractice.repository;
 
-public class ItemRepository {
+import jaein.crudpractice.domain.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select i from Item i where name = :name")
+    List<Item> findByName(@Param("name") String name);
 }
--- src/main/java/jaein/crudpractice/repository/StudentRepository.java
+++ src/main/java/jaein/crudpractice/repository/StudentRepository.java
@@ -1,50 +1,25 @@
 package jaein.crudpractice.repository;
 
 import jaein.crudpractice.domain.Student;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    @PersistenceContext
-    private final EntityManager em;
+    @Query("select s from Student s where s.name = :name")
+    List<Student> findByName(@Param("name") String name);
 
-    public void save(Student student){
-        em.persist(student);
-    }
+    @Query("select s from Student s where s.num = :num")
+    List<Student> findByNum(@Param("num") int num);
 
-    public Student findOne(Long id){
-        return em.find(Student.class, id);
-    }
+    @Query("select s from Student s where s.dept = :dept")
+    List<Student> findByDept(@Param("dept") String dept);
 
-    public List<Student> findAll(){
-        return em.createQuery("select s from Student s", Student.class)
-                .getResultList();
-    }
-
-    public List<Student> findByName(String name){
-        return em.createQuery("select s from Student s where s.name = :name", Student.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
-
-    public List<Student> findByNum(int num){
-        return em.createQuery("select s from Student s where s.num = :num", Student.class)
-                .setParameter("num", num)
-                .getResultList();
-    }
-
-    public List<Student> findByDept(String dept){
-        return em.createQuery("select s from Student s where s.dept = :dept", Student.class)
-                .setParameter("dept", dept)
-                .getResultList();
-    }
-
+    @Query("select s from Student s where s.name = :name and s.num = :num")
+    List<Student> findUser(@Param("name") String name, @Param("num") int num);
 }
