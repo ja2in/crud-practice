f91af14e4f3dd113d30229c4513b7efb929f3e4d
jeongjaein <jaein1608@gmail.com>
2023-12-14 16:19:11 +0900

    feat: add OrderRepository

--- src/main/java/jaein/crudpractice/repository/OrderRepository.java
+++ src/main/java/jaein/crudpractice/repository/OrderRepository.java
@@ -1,4 +1,17 @@
 package jaein.crudpractice.repository;
 
-public class OrderRepository {
+import jaein.crudpractice.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o from Order o join o.student s where s.name = :name")
+    List<Order> findByStudentName(@Param("name") String name);
+
 }
