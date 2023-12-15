f71805529a702c3135f1e7a82b92039913106118
jeongjaein <jaein1608@gmail.com>
2023-12-15 17:06:10 +0900

    feat: add none constructor

--- src/main/java/jaein/crudpractice/domain/item/Book.java
+++ src/main/java/jaein/crudpractice/domain/item/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter @Setter
@@ -12,6 +13,9 @@ public class Book extends Item{
 
     private String author;
     private String isbn;
+
+    public Book() {
+    }
 
     public Book(String author, String isbn) {
         this.author = author;
