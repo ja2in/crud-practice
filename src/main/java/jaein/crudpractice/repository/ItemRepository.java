9baff056c09b9cedf21a163360462d9f931043a3
jeongjaein <jaein1608@gmail.com>
2023-12-15 15:18:00 +0900

    feat: add findQuery

--- src/main/java/jaein/crudpractice/repository/ItemRepository.java
+++ src/main/java/jaein/crudpractice/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package jaein.crudpractice.repository;
 
+import jaein.crudpractice.domain.item.Book;
 import jaein.crudpractice.domain.item.Item;
+import jaein.crudpractice.domain.item.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,17 @@ import java.util.List;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    @Query("select i from Item i where name = :name")
-    List<Item> findByName(@Param("name") String name);
+    /**
+     * Book Query
+     */
+    @Query("select b from Book b where author = :author")
+    List<Book> findByAuthor(@Param("author") String author);
+    @Query("select b from Book b where isbn = :isbn")
+    List<Book> findByIsbn(@Param("isbn") String isbn);
+
+    /**
+     * Movie Query
+     */
+    @Query("select m from Movie m where director = :director")
+    List<Movie> findByDirector(@Param("director") String director);
 }
