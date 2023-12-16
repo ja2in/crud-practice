5e59c22b6deea285bed6051a9775d72d64be49c4
jeongjaein <jaein1608@gmail.com>
2023-12-16 16:41:49 +0900

    feat: add removeStock/addStock

--- src/main/java/jaein/crudpractice/domain/item/Item.java
+++ src/main/java/jaein/crudpractice/domain/item/Item.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Getter @Setter
@@ -24,4 +25,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    public void removeStock(int count){
+        int rest = this.stockQuantity -= count;
+        if(rest < 0){
+            throw new IllegalStateException("no such quantity");
+        }
+        this.stockQuantity = rest;
+    }
+    public void addStock(int count) {
+        this.stockQuantity += count;
+    }
 }
