f545667a4f6193bb76cadb30cfd0d7b73aea69aa
jeongjaein <jaein1608@gmail.com>
2024-01-08 15:56:59 +0900

    feat: OrderApi를 Dto를 사용하여 구현

--- src/main/java/jaein/crudpractice/api/OrderApiController.java
+++ src/main/java/jaein/crudpractice/api/OrderApiController.java
@@ -1,8 +1,74 @@
 package jaein.crudpractice.api;
 
-import org.springframework.stereotype.Controller;
+import jaein.crudpractice.domain.Order;
+import jaein.crudpractice.domain.OrderItem;
+import jaein.crudpractice.repository.OrderRepository;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAll();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+현
+        return result;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findWithItem();
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class OrderDto{
+
+        private Long orderId;
+        private String name;
+        private Date loanDate;
+        private Date returnDate;
+//        private OrderStatus orderStatus;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getStudent().getName();
+            loanDate = order.getLoanDate();
+            returnDate = order.getReturnDate();
+//            orderStatus = order.getStatus();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+
+        private String itemName;
+        private int count;
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            count = orderItem.getCount();
+        }
+    }
 }
